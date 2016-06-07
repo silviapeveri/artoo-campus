f8162ec96352c482a82cf6d9fa7f26bc81db2a31
Silvia Peveri <silvia.peveri01@gmail.com>
2016-06-07 16:27:27 +0000

    subjects Init

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,23 @@
 // define app module
+var app = angular
+.module("artooInit", [])
+.run(function() {
+    console.info('I run in the setter!');
+});
+
+//.config(angular.noop)
+
+// run versione 5
+angular
+.module('artooInit')
+.run(() => {
+    console.info('I run in the getter!');
+});
+
+
+
 // define a run function
+
+app.run(function(){
+    console.info('I run from the module in the var!');
+});
