9732e742a147decfed5c55d7ab4845673705d96a
Silvia Peveri <silvia.peveri01@gmail.com>
2016-06-08 10:34:54 +0000

    add exs to subjects/controllers

--- client/subjects/controllers/item.controller.js
+++ client/subjects/controllers/item.controller.js
@@ -1,4 +1,27 @@
 // create a controller for the items
+angular.module('artoo').controller('ItemsCtrl', ($scope) => {
+    console.log('ItemsCtrl runs!');
+    
+   $scope.item = {
+        name: "Ascia bipenne",
+        description: "L\’ascia bipenne è una scure a due lame, simbolo del potere minoico.",
+        slots: 3,
+        image: 'http://khazalidgrungron.altervista.org/axes/05.png',
+    };
+    
+    $scope.checkIfFits = (item, bagSize) => {
+        return item.slots <= bagSize;
+    };
+    
+    $scope.dispose = () => {
+        $scope.item = undefined;
+    }; //dopo la funzione anonima è necessario il terminatore ;
+    
+    $scope.toggleDescription = () => {
+        $scope.visibleDescription = !$scope.visibleDescription;
+    };
+    
+});
 // attach the following data to it
 
 /*
@@ -9,3 +32,5 @@
  */
 
 // attach some behaviours to the controller: read-description, check-if-fits, dispose
+
+
---
+++ client/subjects/controllers/player.controller.js
@@ -0,0 +1,39 @@
+angular.module('artoo').controller('PlayerCtrl', ($scope) => {
+   $scope.player = {
+       name: 'Legolas',
+       hp: 100,
+       exp: 17,
+       level:2,
+       race: 'elf',
+   };
+    // take damage
+    $scope.takeDamage = (damage) => {
+       $scope.player.hp =  ($scope.player.hp <= damage) ? $scope.player.hp - damage : 0;
+    };
+    
+    // is dead?
+    
+    $scope.isDead = () => {
+        return ($scope.player.hp) ? 'no' : 'yes';
+    };
+   
+    // level up
+    $scope.levelUp = () => {
+        $scope.player.level += 1 ;
+    };
+    
+    // gain exp
+    $scope.gainExp = (exp) => {
+        var totalExp= $scope.player.exp + exp;
+        
+        if (totalExp >= 100){
+            $scope.levelUp();
+            totalExp = totalExp - 100;
+        }
+        
+        $scope.player.exp = totalExp;
+        
+    } ;
+   
+   
+});
