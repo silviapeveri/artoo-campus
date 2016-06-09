d4e1b86356d3a02c1c5ab988b55da27632ee7566
Silvia Peveri <silvia.peveri01@gmail.com>
2016-06-09 17:25:03 +0000

    form exs

--- client/subjects/forms/items.controller.js
+++ client/subjects/forms/items.controller.js
@@ -1,3 +1,38 @@
-angular.module('artoo').controller('ItemsCtrl', function ($scope) {
-  $scope.items = [];
+angular.module('artoo').controller('ItemsCtrl', function ($scope, ItemsSrv) {
+  
+  $scope.ItemsSrv = ItemsSrv;
+  var master;
+
+  
+  $scope.selectItem = function(code) {
+   var master = $scope.item = angular.copy(ItemsSrv.find(code));
+  };
+  
+  $scope.selectedItem = ItemsSrv.get()[0].code;
+  $scope.selectItem($scope.selectedItem);
+  
+  $scope.insert = function(item, form){
+      ItemsSrv.add(item);
+      $scope.resetForm(form);
+      //$scope.resetForm(form);
+  };
+ 
+  $scope.resetForm = function (form){
+      $scope.item = angular.copy(master);
+        // $scope.item = {};
+        // $scope.item = angular.copy(master);
+    }; 
+    
+    
+    $scope.update = function (item, form) {
+       ItemsSrv.update(item);
+       master = angular.copy(item);
+    };    
+        
+    
+    
+   
 });
+
+
+
