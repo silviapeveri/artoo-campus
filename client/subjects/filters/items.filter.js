c84a71488147b544b1a8e54ab109f5c61b514049
Silvia Peveri <silvia.peveri01@gmail.com>
2016-06-10 09:38:13 +0000

    filters update

--- client/subjects/filters/items.filter.js
+++ client/subjects/filters/items.filter.js
@@ -24,5 +24,18 @@ angular.module('artoo')
     var race = PlayerSrv.get().race;
     return function (input){
         return input.indexOf(race) > -1; // -1 è l'indice che non esiste, quando non trova un oggetto.
+    };
+})
+
+    .filter('races', function(){
+    return function (input) {
+        return input.join(', ')+'.';
+    };
+})
+
+    .filter('suitability', function(PlayerSrv){
+    var race = PlayerSrv.get().race;
+    return function (input, active){
+    return (active && input.filter(singleItem => singleItem.races.indexOf(race) > -1)) || input ;
     }
-    })
+})
