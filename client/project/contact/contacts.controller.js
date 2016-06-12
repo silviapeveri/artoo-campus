9eeafaba2983ceb2ba163bffc355b7ce2d058913
Silvia Peveri <silvia.peveri01@gmail.com>
2016-06-12 09:05:34 +0000

    project fix: newContact form reset

--- client/project/contact/contacts.controller.js
+++ client/project/contact/contacts.controller.js
@@ -1,3 +1,8 @@
 angular.module('appenda').controller('ContactsCtrl', function ($scope, ContactsSrv) {
   $scope.ContactsSrv = ContactsSrv;
+  
+  $scope.add = function (newContact) {
+    ContactsSrv.create(newContact);
+    $scope.newContact = {};
+  };
 });
--- client/project/contact/contacts.service.js
+++ client/project/contact/contacts.service.js
@@ -1,7 +1,9 @@
 angular.module('appenda').service('ContactsSrv', function () {
   
+  // Define a maximum id highest id for the contacts objects
   var maxId = 1;
   
+  // Initialize contacts list
   var contacts = [{
     id: 0,
     name: 'Silvia',
@@ -11,16 +13,22 @@ angular.module('appenda').service('ContactsSrv', function () {
     img: 'http://placehold.it/20x20',
   },];
   
+  // create method to insert new contact in contacts list:
+  // gets the compiled form fields converted in an object as input
   this.create = function (formInput) {
-    formInput.id = maxId;
-    maxId = (maxId) ? maxId + 1 : maxId;
-    contacts.push(formInput);
+    formInput.id = maxId++;
+    contacts.push(angular.copy(formInput));
   };
   
+
+  
+  // get method to access contacts list
   this.get = function () {
     return contacts;
   };
   
+  // update method to edit a single contact
+  // gets the compiled form fields converted in an object as input
   this.update = function (formInput) {
     contacts.forEach(function (singleContact) {
       if (singleContact.id === formInput.id) {
@@ -29,6 +37,8 @@ angular.module('appenda').service('ContactsSrv', function () {
     });
   };
   
+  // delete method to remove a contact from the list:
+  // gets the id of the object to remove as input
   this.delete = function (id) {
     contacts.forEach(function (singleContact) {
       if (id === singleContact.id) {
