9e592f493f8bde54bcb2ed4c585bbe182b62d35f
Silvia Peveri <silvia.peveri01@gmail.com>
2016-06-14 06:55:32 +0000

    project features: filter events and contacts

--- client/project/contact/contacts.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('appenda').controller('ContactsCtrl', function ($scope, ContactsSrv) {
-  $scope.ContactsSrv = ContactsSrv;
-  
-  $scope.add = function (newContact) {
-    ContactsSrv.create(newContact);
-    $scope.newContact = {};
-  };
-});
